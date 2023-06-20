322203d8da2ab7c55a268b930ff4495c3946637c
Nabil160 <nbakhunjee97@gmail.com>
2023-06-20 14:17:24 +0600

    product list view re-designed

--- lib/view/product_list_view.dart
+++ lib/view/product_list_view.dart
@@ -12,17 +12,17 @@ class ProductListView extends StatefulWidget {
 
 class _ProductListViewState extends State<ProductListView> {
 
-  List<Product> product = [];
+  List<Product> products = [];
 
-  void initializeProduct() {
-    for (int i = 1; i <= 40; i++) {
-      product.add(Product(name: 'Product $i', price: i * 10));
+  void initializeDummyProducts() {
+    for (int i=1; i<=25; i++) {
+      products.add(Product(name: 'Product $i', price: i*10));
     }
   }
 
   @override
   void initState() {
-    initializeProduct();
+    initializeDummyProducts();
     super.initState();
   }
 
@@ -31,88 +31,93 @@ class _ProductListViewState extends State<ProductListView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Product List'),
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.more_vert)
+          )
+        ],
       ),
       body: ListView.builder(
-        physics: const BouncingScrollPhysics(),
-        itemCount: product.length,
-        itemBuilder: (context, index) {
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-              side: const BorderSide(
-                width: 1.25,
-                color: Colors.blue
-              )
-            ),
-            elevation: 2,
-            child: ListTile(
-              title: Text(product[index].name.toString(),
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black
-                ),
+          physics: const BouncingScrollPhysics(),
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            return Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  side: const BorderSide(
+                      width: 1.25,
+                      color: Colors.blue
+                  )
               ),
-              subtitle: Text('\$ ${product[index].price}',
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w300,
-                  color: Colors.black
-                ),
-              ),
-              trailing: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Expanded(
-                    child: Text(product[index].inCartCount.toString(),
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500
-                      ),
-                    ),
+              elevation: 2,
+              child: ListTile(
+                title: Text(products[index].name.toString(),
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black
                   ),
-                  Expanded(
-                    child: ElevatedButton(
+                ),
+                subtitle: Text('\$ ${products[index].price}',
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black
+                  ),
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
                       onPressed: (){
+                        SnackBar snackBar = const SnackBar(
+                          content: Text('Cannot remove anymore!'),
+                          backgroundColor: Colors.redAccent,
+                        );
                         setState(() {
-                          product[index].inCartCount++;
-
-                          if(product[index].inCartCount == 5) {
-                            showDialog(
-                              context: context,
-                              barrierDismissible: false,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: const Text('Congratulations!'),
-                                  content: Text('You\'ve bought 5 ${product[index].name}!'),
-                                  actions: [
-                                    TextButton(
-                                      child: const Text('Ok'),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          }
+                          products[index].inCartCount > 0
+                              ? products[index].inCartCount--
+                              : ScaffoldMessenger.of(context)
+                              .showSnackBar(snackBar);
                         });
                       },
-                      child: const Icon(Icons.add),
+                      child: Icon(Icons.remove),
+                      style: ElevatedButton.styleFrom(
+                          shape: CircleBorder()
+                      ),
                     ),
-                  )
-                ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 2),
+                      child: Text(products[index].inCartCount.toString(),
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: (){
+                        setState(() {
+                          products[index].inCartCount++;
+                        });
+                      },
+                      child: Icon(Icons.add),
+                      style: ElevatedButton.styleFrom(
+                          shape: CircleBorder()
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          );
-        }
+            );
+          }
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           int totalProductInCart = 0;
-          for (var i=0; i<product.length; i++){
-            totalProductInCart += product[i].inCartCount;
+          for (var i=0; i<products.length; i++){
+            totalProductInCart += products[i].inCartCount;
           }
 
           Navigator.push(
@@ -120,8 +125,8 @@ class _ProductListViewState extends State<ProductListView> {
             MaterialPageRoute(builder: (context) => CartView(totalProductInCart)),
           );
         },
+        child: Icon(Icons.shopping_cart),
         elevation: 5,
-        child: const Icon(Icons.shopping_cart),
       ),
     );
   }
